857720abb4e1543ff0bd542ff1daff7ae76256e4
Hiyam G <hkg02@mail.aub.edu>
2024-10-30 10:15:03 +0200

    added updated prompts and code to loop over events (+3 days)

--- POST-THESIS/LLMs/main.py
+++ POST-THESIS/LLMs/main.py
@@ -24,21 +24,46 @@ def get_response(text):
     return response.strip()  # Strip any leading/trailing whitespace
 
 
+def mkdir(folder_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+
+# mapping of the events to the first 3 days of the file names
+events = {
+    'Philip Habib Negotiations': ['820607', '820608', '820609'],
+    'Alexander Haig resignation': ['820625', '820626', '820627'],
+    'The PLO approves Philip Habibs initiative to withdraw from Lebanon': ['820807', '820808', '820809'],
+    'Bachir Gemayel Election': ['820823', '820824', '820825'],
+    'Arafat and the PLO withdraw from Beirut': ['820914', '820915', '820916'],
+    'Sabra and Shatila Massacre': ['820915', '820916', '820917'],
+    'Election of Amine Gemayel': ['820921', '820922', '820923']
+}
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="LLMs for Hate & Sectarian Speech Detection")
     parser.add_argument("--model_name", type=str, default="core42/jais-13b", help="The model to use for prediction")
     parser.add_argument("--prompt_file", type=str, default="prompts/prompt0.txt", help="The model to use for prediction")
     args = parser.parse_args()
 
-    final_chunks = []
-    root_dir = 'txts/'
-    for file_name in os.listdir('txts/'):
-        if file_name.endswith('.txt'):
-            with open(os.path.join(root_dir, file_name), encoding='utf-8') as f:
-                lines = f.readlines()
-                chunks = [lines[i: i + 4] for i in range(0, len(lines), 4)]
-                for chunk in chunks:
-                    final_chunks.append('\n'.join([l.replace('\n', '') for l in chunk]))
+    rootdir = '/onyx/data/p118/POST-THESIS/generate_bert_embeddings/opinionated_articles_DrNabil/1982/txt_files/'
+    archives = ['An-Nahar', 'As-Safir']
+
+    final_chunks = {}
+    for archive in archives:
+        final_chunks[archive] = {}
+        for event in events:
+            final_chunks[archive][event] = []
+            path = os.path.join(rootdir, archive)
+            for file in os.listdir(path):
+                if '.txt' in file and any([substr in file for substr in events[event]]):
+                    with open(os.path.join(path, file), encoding='utf-8') as f:
+                        lines = f.readlines()
+                        chunks = [lines[i: i + 4] for i in range(0, len(lines), 4)]
+                        for c in chunks:
+                            fc = '\n'.join([l.replace('\n', '') for l in c])
+                            print(file, fc)
+                            final_chunks[archive][event].append(fc)
 
     login(token="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")
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -46,9 +71,18 @@ if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto", trust_remote_code=True)
 
-    for text_chunk in final_chunks:
-        with open(args.prompt_file, encoding="utf-8") as f:
-            prompt = f.read().format(text_chunk)
+    for archive in archives:
+        for event in events:
+            output_dir = 'output/{}/{}/{}-{}/'.format(archive, event, args.model_name.replace("/", "-"), args.prompt_file.replace("prompts/", "").replace(".txt", ""))
+            mkdir(output_dir)
 
-        print(get_response(prompt))
-        print('====================================================================')
+            for text_chunk in final_chunks[archive][event]:
+                with open(args.prompt_file, encoding="utf-8") as f:
+                    prompt = f.read().format(text_chunk)
+
+                response = get_response(prompt)
+                print(response)
+                with open(os.path.join(output_dir, 'output.txt'), 'a', encoding='utf-8') as f:
+                    f.write(response + '\n')
+                    f.write('===================================================\n\n')
+                f.close()
