637c4106e1197a1cea68544ce48ae30051f0c83d
Hiyam G <hkg02@mail.aub.edu>
2024-12-02 11:58:08 +0200

    refactored bias quantification code to take from weekly time-sepcific embeddings

--- POST-THESIS/bias_quantification/main.py
+++ POST-THESIS/bias_quantification/main.py
@@ -8,7 +8,7 @@ from bidi.algorithm import get_display
 import argparse
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 from normalization import ArabicNormalizer
-
+from datetime import date
 
 
 def mkdir(direc):
@@ -45,42 +45,44 @@ def plot_differences(list1, list2, model, features, xlabel, ylabel, save_dir, fi
     # return C, top_idx
 
 
-def plot_embedding_bias_over_time(biases, ylabel, event_name, distance_type, save_dir):
-    for model_name in biases:
-        for archive_name in biases[model_name]:
-            print(biases[model_name][archive_name])
-            plt.plot(list(range(len(biases[model_name][archive_name]))), biases[model_name][archive_name], marker='o', label=f"EB for {archive_name}")
+def plot_embedding_bias_over_time(biases, years, ylabel, event_name, distance_type, save_dir):
+    # Step 1: Create a unified list of years
+    unified_years = sorted(set(years[0]).union(set(years[1])))
 
-        plt.hlines(y=0, xmin=0, xmax=len(biases[model_name][archive_name]), colors='black', linestyles='--', lw=2, label='EB=0')
+    for archive_name in biases:
+        print(biases[archive_name])
 
-        plt.legend()
+        aligned_biases = []
+        archive_years = years[0] if archive_name == "An-Nahar" else years[1]
+        year_to_bias = dict(zip(archive_years, biases[archive_name]))
 
-        fig = plt.gcf()
-        fig.set_size_inches(12, 6)
-        plt.grid(axis='y')
+        for year in unified_years:
+            aligned_biases.append(year_to_bias.get(year, None))  # None if year is missing
 
-        plt.xticks(list(range(len(biases[model_name][archive_name]))), ['1982-{}'.format(m) for m in ['06', '07', '08', '09', '10', '11', '12']], rotation=45)
-        plt.ylabel(ylabel)
+        # Filter out None values for plotting
+        valid_indices = [i for i, b in enumerate(aligned_biases) if b is not None]
+        valid_biases = [aligned_biases[i] for i in valid_indices]
 
-        mkdir(save_dir)
-        fig_name = '{}_{}_{}'.format(event_name, model_name, distance_type)
-        plt.savefig(os.path.join(save_dir, fig_name + '.png'), dpi=300)
-        plt.close()
+        # plt.plot(list(range(len(biases[model_name][archive_name]))), biases[model_name][archive_name], marker='o', label=f"EB for {archive_name}")
+        plt.plot(valid_indices, valid_biases, marker='o', label=f"EB for {archive_name}")
 
+    plt.hlines(y=0, xmin=0, xmax=len(unified_years) - 1, colors='black', linestyles='--', lw=2, label='EB=0')
 
-# def get_word_embedding(word, tokenizer):
-#     # Tokenize the word
-#     inputs = tokenizer(word, return_tensors="pt")
-#     input_ids = inputs["input_ids"].cuda()
-#
-#     # Get hidden states
-#     with torch.no_grad():
-#         outputs = model(input_ids)
-#         hidden_states = outputs.hidden_states  # All layers' hidden states
-#
-#     # Use the last 4 layers and take the mean for the word embedding
-#     word_embedding = torch.stack(hidden_states[-4:]).mean(0)[0, 1:-1].mean(0)  # Ignore [CLS] and [SEP] tokens
-#     return word_embedding.cpu().numpy()
+    plt.legend()
+
+    fig = plt.gcf()
+    fig.set_size_inches(12, 6)
+    plt.grid(axis='y')
+
+    # plt.xticks(list(range(len(biases[model_name][archive_name]))), ['1982-{}'.format(m) for m in years], rotation=45)
+    plt.xticks(list(range(len(unified_years))), [str(year) for year in unified_years], rotation=45)
+    plt.ylabel(ylabel)
+
+    mkdir(save_dir)
+    fig_name = '{}_{}_{}'.format(event_name, model_name, distance_type)
+    plt.savefig(os.path.join(save_dir, fig_name + '.png'), dpi=300)
+    plt.close()
+
 
 def get_word_embedding_static(word, tokenizer, model):
     tokenized = tokenizer(word, return_tensors="pt", add_special_tokens=True)
@@ -214,9 +216,33 @@ def get_time_specific_word_embedding(word, year, embeddings, tokenizer, model):
         return embedding
 
 
-def embedding_bias(embeddings, target_list1, target_list2, neutral_list, distance_type, tokenizer, model, device="cpu"):
+def get_week_coverage_sorted(archive):
+    """
+    loops over the .txt files and returns a sorted list of months-weeknbs
+    """
+    months_weeks = set()
+    root_dir = '/onyx/data/p118/POST-THESIS/generate_bert_embeddings/opinionated_articles_DrNabil/1982/txt_files/{}/'.format(archive)
+    for file in os.listdir(root_dir):
+        month = file[2:4]
+        day = file[4:6]
+        year = "1982"
+        print(f'processing file {file} with year: {year}, month: {month}, day: {day}')
+        given_date = date(int(year), int(month), int(day))
+        week_number = given_date.isocalendar()[1]
+
+        month_week = f"{month}_{str(week_number)}"
+
+        months_weeks.add(month_week)
+
+    months_weeks_l = list(months_weeks)
+    months_weeks_ls = sorted(months_weeks_l, key=lambda x: (int(x.split('_')[0]), int(x.split('_')[1])))
+    print(f"months_weeks sorted for {archive}: {months_weeks_ls}")
+
+    return months_weeks_ls
+
+
+def embedding_bias(embeddings, years, target_list1, target_list2, neutral_list, distance_type, tokenizer, model, device="cpu"):
     means = []
-    years = ['06', '07', '08', '09', '10', '11', '12']
     for year in years:
         v1, v2 = [], []
         for word in target_list1:
@@ -269,8 +295,13 @@ def embedding_bias(embeddings, target_list1, target_list2, neutral_list, distanc
     return means
 
 
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str, default='UBC-NLP-MARBERTv2', help='name of the model for which embeddings are stored')
+    parser.add_argument("--split_by", type=str, default="weekly", help="either `monthly` or `weekly`")
+    parser.add_argument("--disttype", type=str, default="cossim", help="distance measure")
+    args = parser.parse_args()
+
     entities_target_neutral_lists = {
         'Arab World Slacktivism': {
             'neutral_list': '../generate_bert_embeddings/entities/bias_quantification/entities1.txt',
@@ -309,86 +340,77 @@ if __name__ == '__main__':
         }
     }
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=str, default='UBC-NLP-MARBERTv2', help='name of the model for which embeddings are stored')
-    args = parser.parse_args()
+    months_weeknbs_nahar = get_week_coverage_sorted(archive="An-Nahar")
+    months_weeknbs_assafir = get_week_coverage_sorted(archive="As-Safir")
 
-    models = [
-        "UBC-NLP-MARBERTv2",
-        "qarib-bert-base-qarib",
-        "UBC-NLP-MARBERT",
-        "UBC-NLP-ARBERT",
-        "aubmindlab-bert-base-arabert",
-        "aubmindlab-bert-base-arabertv02-twitter",
-        "aubmindlab-bert-base-arabertv2",
-        "aubmindlab-bert-base-arabertv01",
-        "aubmindlab-bert-base-arabertv02",
-    ]
+    model_name = args.model_name
+    split_by = args.split_by
+    dist_type = args.disttype
 
     for event_name in entities_target_neutral_lists:
         biases = {}
-        for dist_type in ['norm', 'cossim']:
-            for model_name in models:
-                biases[model_name] = {}
 
-                path_nahar = '/onyx/data/p118/POST-THESIS/generate_bert_embeddings/opinionated_articles_DrNabil/1982/embeddings/An-Nahar/{}/'.format(model_name)
-                path_assafir = '/onyx/data/p118/POST-THESIS/generate_bert_embeddings/opinionated_articles_DrNabil/1982/embeddings/As-Safir/{}/'.format(model_name)
+        path_nahar = '/onyx/data/p118/POST-THESIS/generate_bert_embeddings/opinionated_articles_DrNabil/1982/embeddings/An-Nahar/{}/{}/'.format(model_name, split_by)
+        path_assafir = '/onyx/data/p118/POST-THESIS/generate_bert_embeddings/opinionated_articles_DrNabil/1982/embeddings/As-Safir/{}/{}/'.format(model_name, split_by)
 
-                path_to_model_nahar = "/onyx/data/p118/POST-THESIS/generate_bert_embeddings/trained_models/An-Nahar/{}/".format(model_name)
-                path_to_model_assafir = "/onyx/data/p118/POST-THESIS/generate_bert_embeddings/trained_models/As-Safir/{}/".format(model_name)
+        path_to_model_nahar = "/onyx/data/p118/POST-THESIS/generate_bert_embeddings/trained_models/An-Nahar/{}/".format(model_name)
+        path_to_model_assafir = "/onyx/data/p118/POST-THESIS/generate_bert_embeddings/trained_models/As-Safir/{}/".format(model_name)
 
-                # tokenizers for each archive
-                tokenizer_nahar = AutoTokenizer.from_pretrained(path_to_model_nahar)
-                tokenizer_assafir = AutoTokenizer.from_pretrained(path_to_model_assafir)
+        # tokenizers for each archive
+        tokenizer_nahar = AutoTokenizer.from_pretrained(path_to_model_nahar)
+        tokenizer_assafir = AutoTokenizer.from_pretrained(path_to_model_assafir)
 
-                # models for each archive
-                model_nahar = AutoModelForMaskedLM.from_pretrained(path_to_model_nahar, output_hidden_states=True).cuda()
-                model_nahar.eval()
+        # models for each archive
+        model_nahar = AutoModelForMaskedLM.from_pretrained(path_to_model_nahar, output_hidden_states=True).cuda()
+        model_nahar.eval()
 
-                model_assafir = AutoModelForMaskedLM.from_pretrained(path_to_model_assafir, output_hidden_states=True).cuda()
-                model_assafir.eval()
+        model_assafir = AutoModelForMaskedLM.from_pretrained(path_to_model_assafir,
+                                                             output_hidden_states=True).cuda()
+        model_assafir.eval()
 
-                device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-                with open(os.path.join(path_nahar, 'embeddings.pickle'), 'rb') as handle:
-                    embeddings_nahar = pickle.load(handle)
-                    print(len(embeddings_nahar))
+        with open(os.path.join(path_nahar, 'embeddings.pickle'), 'rb') as handle:
+            embeddings_nahar = pickle.load(handle)
+            print(len(embeddings_nahar))
 
-                with open(os.path.join(path_assafir, 'embeddings.pickle'), 'rb') as handle:
-                    embeddings_assafir = pickle.load(handle)
-                    print(len(embeddings_assafir))
+        with open(os.path.join(path_assafir, 'embeddings.pickle'), 'rb') as handle:
+            embeddings_assafir = pickle.load(handle)
+            print(len(embeddings_assafir))
 
-                print(f"Processing {event_name}")
-                file_entities = entities_target_neutral_lists[event_name]['neutral_list']
-                file_negconn = entities_target_neutral_lists[event_name]['target_list'][0]
-                file_posconn = entities_target_neutral_lists[event_name]['target_list'][1]
+        print(f"Processing {event_name}")
+        file_entities = entities_target_neutral_lists[event_name]['neutral_list']
+        file_negconn = entities_target_neutral_lists[event_name]['target_list'][0]
+        file_posconn = entities_target_neutral_lists[event_name]['target_list'][1]
 
-                with open(file_entities, encoding='utf-8') as f:
-                    entities = f.readlines()
-                    entities = [e.strip().replace("\n", "") for e in entities]
+        with open(file_entities, encoding='utf-8') as f:
+            entities = f.readlines()
+            entities = [e.strip().replace("\n", "") for e in entities]
 
-                with open(file_negconn, encoding='utf-8') as f:
-                    negconn = f.readlines()
-                    negconn = [e.strip().replace("\n", "") for e in negconn]
+        with open(file_negconn, encoding='utf-8') as f:
+            negconn = f.readlines()
+            negconn = [e.strip().replace("\n", "") for e in negconn]
 
-                with open(file_posconn, encoding='utf-8') as f:
-                    posconn = f.readlines()
-                    posconn = [e.strip().replace("\n", "") for e in posconn]
+        with open(file_posconn, encoding='utf-8') as f:
+            posconn = f.readlines()
+            posconn = [e.strip().replace("\n", "") for e in posconn]
 
+        biases_nahar = embedding_bias(embeddings_nahar, years=months_weeknbs_nahar, target_list1=negconn,
+                                      target_list2=posconn, neutral_list=entities, distance_type=dist_type,
+                                      tokenizer=tokenizer_nahar, model=model_nahar)
+        biases['An-Nahar'] = biases_nahar
 
-                biases_nahar = embedding_bias(embeddings_nahar, target_list1=negconn, target_list2=posconn, neutral_list=entities, distance_type=dist_type,
-                                              tokenizer=tokenizer_nahar, model=model_nahar)
-                biases[model_name]['An-Nahar'] = biases_nahar
+        biases_assafir = embedding_bias(embeddings_assafir, years=months_weeknbs_assafir, target_list1=negconn,
+                                        target_list2=posconn, neutral_list=entities, distance_type=dist_type,
+                                        tokenizer=tokenizer_assafir, model=model_assafir)
+        biases['As-Safir'] = biases_assafir
+        # read each word
+        # for event_name in entities_target_neutral_lists:
 
-                biases_assafir = embedding_bias(embeddings_assafir, target_list1=negconn, target_list2=posconn, neutral_list=entities, distance_type=dist_type,
-                                                tokenizer=tokenizer_assafir, model=model_assafir)
-                biases[model_name]['As-Safir'] = biases_assafir
-                # read each word
-                # for event_name in entities_target_neutral_lists:
-
-            plot_embedding_bias_over_time(biases=biases,
-                                          ylabel=f"Avg. Embedding Bias for {event_name}",
-                                          event_name=event_name,
-                                          distance_type=dist_type,
-                                          save_dir="plots/"
-                                          )
+        plot_embedding_bias_over_time(biases=biases,
+                                      years=[months_weeknbs_nahar, months_weeknbs_assafir],
+                                      ylabel=f"Avg. Embedding Bias for {event_name}",
+                                      event_name=event_name,
+                                      distance_type=dist_type,
+                                      save_dir="plots/"
+                                      )
