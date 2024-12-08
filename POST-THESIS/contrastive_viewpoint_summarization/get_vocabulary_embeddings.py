9d5cc3d3dfbf313fd879f276314a5ccfba78eeea
Hiyam G <hkg02@mail.aub.edu>
2024-12-08 13:35:15 +0200

    refactored getting vocabulary embeddings per model per archive

--- POST-THESIS/contrastive_viewpoint_summarization/get_vocabulary_embeddings.py
+++ POST-THESIS/contrastive_viewpoint_summarization/get_vocabulary_embeddings.py
@@ -6,6 +6,7 @@ import argparse
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 from normalization import ArabicNormalizer
 from tqdm import tqdm
+from datetime import date
 
 
 def mkdir(folder_name):
@@ -145,16 +146,56 @@ def get_time_specific_word_embedding(word, year, embeddings, tokenizer, model):
         return embedding
 
 
+def get_sorted_files(directory):
+    # Get list of files in the directory
+    files = [os.path.join(directory, file) for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
+
+    try:
+        # Sort files by last modified time
+        files.sort(key=os.path.getmtime, reverse=True)  # reverse=True for descending order
+    except:
+        return []
+
+    return files
+
+
+def get_week_coverage_sorted(archive):
+    """
+    loops over the .txt files and returns a sorted list of months-weeknbs
+    """
+    months_weeks = set()
+    root_dir = '/onyx/data/p118/POST-THESIS/generate_bert_embeddings/opinionated_articles_DrNabil/1982/txt_files/{}/'.format(archive)
+    for file in os.listdir(root_dir):
+        month = file[2:4]
+        day = file[4:6]
+        year = "1982"
+        print(f'processing file {file} with year: {year}, month: {month}, day: {day}')
+        given_date = date(int(year), int(month), int(day))
+        week_number = given_date.isocalendar()[1]
+
+        month_week = f"{month}_{str(week_number)}"
+
+        months_weeks.add(month_week)
+
+    months_weeks_l = list(months_weeks)
+    months_weeks_ls = sorted(months_weeks_l, key=lambda x: (int(x.split('_')[0]), int(x.split('_')[1])))
+    print(f"months_weeks sorted for {archive}: {months_weeks_ls}")
+
+    return months_weeks_ls
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--archive_name", default="An-Nahar", help="name of the archive to compute embeddings for")
-    parser.add_argument("--model_name", default="UBC-NLP-MARBERT", help="model name for archive")
+    parser.add_argument("--model_name", default="aubmindlab-bert-base-arabertv2", help="model name for archive")
+    parser.add_argument("--split_by", default="monthly", help="time-specific embeddings split `weekly` or `monthly`")
     args = parser.parse_args()
 
     archive_name = args.archive_name
     model_name = args.model_name
+    split_by = args.split_by
 
-    path = '/onyx/data/p118/POST-THESIS/generate_bert_embeddings/opinionated_articles_DrNabil/1982/embeddings/{}/{}/'.format(archive_name, model_name)
+    path = '/onyx/data/p118/POST-THESIS/generate_bert_embeddings/opinionated_articles_DrNabil/1982/embeddings/{}/{}/{}/'.format(archive_name, model_name, split_by)
     path_to_model = "/onyx/data/p118/POST-THESIS/generate_bert_embeddings/trained_models/{}/{}/".format(archive_name, model_name)
 
     # tokenizer for the archive
@@ -171,23 +212,63 @@ if __name__ == '__main__':
 
     vocab_words = list(tokenizer.get_vocab().keys())
 
-    years = ['06', '07', '08', '09', '10', '11', '12']
-    vocabs_to_embeddings = {}
+    if split_by == "monthly":
+        years = ['06', '07', '08', '09', '10', '11', '12']
+    else:
+        years = get_week_coverage_sorted(archive_name)
 
-    save_dir = f"vocabulary_embeddings/{archive_name}/{model_name}/"
+    save_dir = f"vocabulary_embeddings/{archive_name}/{model_name}-{split_by}/"
     mkdir(save_dir)
 
-    for v in tqdm(vocab_words):
+    all_vocabs_sofar = {}
+    for file in tqdm(os.listdir(save_dir)):
+        if "pickle" in file:
+            with open(os.path.join(save_dir, file), 'rb') as handle:
+                try:
+                    vocab_emb = pickle.load(handle)
+                    for k in vocab_emb:
+                        all_vocabs_sofar[k] = vocab_emb[k]
+                except:
+                    pass
+
+    vocabs_to_embeddings = {}
+    files = get_sorted_files(directory=save_dir)
+    batch_num = 0
+    if files:
+        batch_num = files[0].replace(args.archive_name, "").replace(args.model_name, "").replace("vocabulary_embeddings", "").replace("/", "").split("_")[0]
+    print(f"Found this as the latest batch number: {batch_num}")
+
+    for i, v in enumerate(tqdm(vocab_words)):
+
         for year in years:
+
+            word_year = f"{v}_{year}"
+
+            if word_year in all_vocabs_sofar:
+                continue
+
             v_emb_y = get_time_specific_word_embedding(word=v, year=year, embeddings=embeddings, tokenizer=tokenizer, model=model)
 
             if isinstance(v_emb_y, np.ndarray): # if numpy array make it a tensor
                 v_emb_y = torch.tensor(v_emb_y).to(device)
+
             if v_emb_y.shape != (1, 768):       # ensure all vectors are of shape (1, 768)
                 v_emb_y = v_emb_y.unsqueeze(0).to(device)
 
-            vocabs_to_embeddings[f"{v}_{year}"] = v_emb_y
+            # vocabs_to_embeddings = {word_year: v_emb_y}
+            vocabs_to_embeddings[word_year] = v_emb_y
+            if len(vocabs_to_embeddings) >= 1000:
+                batch_num = int(batch_num) + 1
 
-            with open(os.path.join(save_dir, f'{archive_name}_{model_name}.pickle'), 'wb') as handle:
-                pickle.dump(vocabs_to_embeddings, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                file_name = f"{batch_num}_batch.pickle"
+                with open(os.path.join(save_dir, file_name), 'wb') as handle:
+                    pickle.dump(vocabs_to_embeddings, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                vocabs_to_embeddings = {}
+
+    if vocabs_to_embeddings:
+        batch_num = int(batch_num) + 1
+        file_name = f"{batch_num}_batch.pickle"
+        with open(os.path.join(save_dir, file_name), 'wb') as handle:
+            pickle.dump(vocabs_to_embeddings, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
 
